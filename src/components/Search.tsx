f27dc9ce4d9c8ff36a69e2dd8c05ccc3fc0bd404
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-05-02 22:54:45 -0300

    Implementando search itens

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,36 +1,69 @@
 import { MagnifyingGlass } from "phosphor-react";
 import { SearchItens } from "./Search-Itens";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type SearchProps =
+  | {
+      id: number;
+      name: string;
+    }[]
+  | null;
+
+type SearchItensProps = {
+  search: string;
+};
 
 export function Search() {
-  const [search, setSearch] = useState("");
+  const { register, handleSubmit } = useForm<SearchItensProps>();
+  const [words, setWords] = useState<SearchProps>(null);
+
+  function handleOnSubmit(data: SearchItensProps) {
+    const cloneWords = [...(words || [])];
+    setWords([
+      ...cloneWords,
+      {
+        id: Math.floor(Math.random() * 100),
+        name: data.search,
+      },
+    ]);
+  }
+
+  function handleOnDelete(id: number) {
+    const cloneWords = [...(words || [])];
+    const newWords = cloneWords.filter((word) => word.id !== id);
+    setWords(newWords);
+  }
   return (
     <div className="card">
-      <div className="search-bar">
+      <form onSubmit={handleSubmit(handleOnSubmit)} className="search-bar">
         <div className="shrink-0 text-base text-gray-500 select-none sm:text-sm/6">
           <MagnifyingGlass className="h-5 w-5" />
         </div>
         <input
           id="search"
-          name="search"
           type="text"
           placeholder="Search..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
           className="block min-w-0 grow  pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+          {...register("search")}
         />
-      </div>
+        <button
+          type="submit"
+          className="bg-blue-900 rounded-md text-white hover:bg-blue-600 transition-colors duration-200 ease-in-out p-1  text-sm"
+        >
+          Search
+        </button>
+      </form>
+
       <div className="flex items-start gap-2 p-1 overflow-x-auto">
-        <SearchItens id="i" name="javascript" key={0} />
-        <SearchItens id="i" name="pyton" key={1} />
-        <SearchItens id="i" name="programação" key={2} />
-        {/* <SearchItens id="i" name="java" key={3} />
-        <SearchItens id="i" name="c++" key={4} />
-        <SearchItens id="i" name="c#" key={5} />
-        <SearchItens id="i" name="php" key={6} />
-        <SearchItens id="i" name="html" key={7} />
-        <SearchItens id="i" name="css" key={8} />
-        <SearchItens id="i" name="typescript" key={9} /> */}
+        {words?.map((word) => (
+          <SearchItens
+            id={word.id}
+            name={word.name}
+            key={word.id}
+            handleOnDelete={handleOnDelete}
+          />
+        ))}
       </div>
     </div>
   );
